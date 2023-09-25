1eb324abd0d542119916160ea8a71ae3e6287015
dinas <dalsalahy@ivytech.edu>
2023-09-25 14:05:21 -0400

    First commit First video min 12

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+const morgan = require('morgan');
+
 
 //Express app
 const app = express();
@@ -11,6 +13,24 @@ app.set ('view engine', 'ejs'); //set lets us set some settings it looks into vi
 
 //listen for request
 app.listen(3000); //starting the server
+
+//Middleware
+
+app.use((req, res, next) => {
+   console.log('new request made:');
+   console.log('host: ', req.hostname);
+   console.log('path: ', req.path);
+   console.log('method: ', req.method);
+   next(); // we have to include that so the browser keep moving and execute the next line.
+ });
+
+ app.use((req, res, next) => { // added this to test the next from the previous function
+   console.log('in the next middleware');
+   next();
+ });
+
+ app.use(morgan('dev'));  // this is what we get on consol from this function (GET / 304 11.549 ms - -)
+
 
 // here app responds to get command it has 2 arguments (what app listens to , function(req,res))  req like get or post , res is what the server responds with
 app.get ('/' , (req, res) => {
